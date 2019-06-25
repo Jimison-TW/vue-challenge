7c1296c1f6a056714844cd0cd02eb124a8c6d14a
Jimison-TW <ex3067878@yahoo.com.tw>
2019-06-25 17:57:33 +0800

    暫存變更

---
+++ src/views/ToDoList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Learn Vuex 2.0 Todo List</h1>
+    <input type="text" placeholder="add Todo.." />
+    <button type="button">新增</button>
+    <div class="center-inline">
+      <div>
+        <h3>Todo List</h3>
+      </div>
+      <div>
+        <h3>Done List</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({})
+}
+</script>
+
+<style>
+.center-inline {
+  margin: 0px auto;
+  text-align: center;
+}
+
+.center-inline div {
+  display: inline-block;
+  width: 150px;
+}
+</style>
