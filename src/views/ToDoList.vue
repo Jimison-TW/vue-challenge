71eff0f47c12e1a6d037df0b9782d5bfaa8df4cd
Jimison-TW <ex3067878@yahoo.com.tw>
2019-06-28 18:00:59 +0800

    暫存變更

--- src/views/ToDoList.vue
+++ src/views/ToDoList.vue
@@ -2,13 +2,26 @@
   <div>
     <h1>Learn Vuex 2.0 Todo List</h1>
     <input type="text" placeholder="add Todo.." />
-    <button type="button">新增</button>
+    <input type="submit" value="新增" @click="addToList()"/>
     <div class="center-inline">
       <div>
-        <h3>Todo List</h3>
+        <h3 class="list-title">Todo List</h3>
+        <ul>
+          <li v-for="todo in todo_list" :key="todo.id" class="list-item">
+            <input type="checkbox" :name="todo" :id="key" /> {{ todo.title }}
+            <button>修改</button>
+            <input type="button" value="刪除" />
+          </li>
+        </ul>
       </div>
       <div>
         <h3>Done List</h3>
+        <ul>
+          <li v-for="done in done_list" :key="done.id">
+            <input type="checkbox" :id="key" />
+            {{ done.title }}
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -16,18 +29,37 @@
 
 <script>
 export default {
-  data: () => ({})
+  data: () => ({
+    todo_list: [
+      { title: 'Vue.js 2.0' },
+      { title: 'Vuex 2.0' },
+      { title: 'Webpack 2.0' }
+    ],
+    done_list: [{ title: 'Vue-router 2.0' }]
+  }),
+  methods: {
+    
+  }
 }
 </script>
 
 <style>
 .center-inline {
   margin: 0px auto;
-  text-align: center;
 }
 
 .center-inline div {
   display: inline-block;
-  width: 150px;
+  vertical-align: top;
+  width: 300px;
+}
+
+.list-title {
+  padding-left: 20px;
+  text-align: left;
+}
+
+.list-item {
+  text-align: left;
 }
 </style>
