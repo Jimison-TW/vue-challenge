48f7660db4241d84b659edb90db709d4959058b8
Jimison-TW <ex3067878@yahoo.com.tw>
2019-07-04 17:58:09 +0800

    完成一半

--- src/views/ToDoList.vue
+++ src/views/ToDoList.vue
@@ -1,24 +1,29 @@
 <template>
   <div>
     <h1>Learn Vuex 2.0 Todo List</h1>
-    <input type="text" placeholder="add Todo.." />
-    <input type="submit" value="新增" @click="addToList()"/>
+    <input type="text" placeholder="add Todo.." v-model="todo_value" />
+    <input type="submit" value="新增" @click="addToList(todo_value)" />
     <div class="center-inline">
       <div>
         <h3 class="list-title">Todo List</h3>
         <ul>
           <li v-for="todo in todo_list" :key="todo.id" class="list-item">
-            <input type="checkbox" :name="todo" :id="key" /> {{ todo.title }}
+            <input
+              type="checkbox"
+              v-model="todo.isClicked"
+              @change="checkFinished(todo.isClicked)"
+            />
+            {{ todo.title }}
             <button>修改</button>
             <input type="button" value="刪除" />
           </li>
         </ul>
       </div>
       <div>
-        <h3>Done List</h3>
+        <h3 class="list-title">Done List</h3>
         <ul>
-          <li v-for="done in done_list" :key="done.id">
-            <input type="checkbox" :id="key" />
+          <li v-for="done in done_list" :key="done.id" class="list-item">
+            <input type="checkbox" />
             {{ done.title }}
           </li>
         </ul>
@@ -30,15 +35,19 @@
 <script>
 export default {
   data: () => ({
+    todo_value: '',
     todo_list: [
-      { title: 'Vue.js 2.0' },
-      { title: 'Vuex 2.0' },
-      { title: 'Webpack 2.0' }
+      { isClicked: false, title: 'Vue.js 2.0' },
+      { isClicked: false, title: 'Vuex 2.0' },
+      { isClicked: false, title: 'Webpack 2.0' }
     ],
     done_list: [{ title: 'Vue-router 2.0' }]
   }),
   methods: {
-    
+    addToList: function(value) {
+      this.todo_list.push({ title: value })
+    },
+    checkFinished: function(isDone) {}
   }
 }
 </script>
