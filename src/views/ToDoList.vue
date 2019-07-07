2e58ab53c5aa2778c77609f1fa9db2b1a806977f
Jimison-TW <ex3067878@gmail.com>
2019-07-07 18:00:27 +0800

    完成修改功能

--- src/views/ToDoList.vue
+++ src/views/ToDoList.vue
@@ -9,9 +9,9 @@
         <ul>
           <li v-for="(todo, index) in todo_list" :key="todo.id" class="list-item">
             <input type="checkbox" v-model="todo.value" @change="checkFinished(index, todo.value)" />
-            <p :hidden="todo.inEdit">{{ todo.title }}</p>
-            <input type="text" v-model="todo.title" :hidden="!todo.inEdit" />
-            <button>修改</button>
+            <span :hidden="todo.inEdit">{{ todo.title }}</span>
+            <input type="text" v-model="todo.title" :hidden="!todo.inEdit" size="15"/>
+            <button @click="editTodo(index)">修改</button>
             <input type="button" value="刪除" @click="removeTodo(index)" />
           </li>
         </ul>
